d24f70c26cf44fe3d34e550c0643fe64f599c663
Tiff Joy <tiffanysummerford@gmail.com>
2020-09-11 14:52:29 -0400

    'added'

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   'migrationDirectory': 'migrations',
   'driver': 'pg',
-  'connectionString': process.env.NODE_ENV === 'test'
+  'connectionString': (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
 };
